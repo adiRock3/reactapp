cd40543e6b630edadab144548d1340be08cf4d9c
adiRock3 <141323421+adiRock3@users.noreply.github.com>
2024-07-21 21:49:52 +0530

    api updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import MovieCard from './moviecard'; // Capitalized to follow React convention
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com?apikey=cb876d2a';
+const API_URL = 'https://www.omdbapi.com?apikey=cb876d2a';
 
 const App = () => {
     const [movies, setMovies] = useState([]);
