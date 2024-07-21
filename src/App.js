9f2ad32127dcc43427beecde9f40795aa5b0bac1
adiRock3 <141323421+adiRock3@users.noreply.github.com>
2024-07-21 21:44:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import MovieCard from './moviecard'; // Capitalized to follow React convention
+import SearchIcon from './search.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const API_URL = 'http://www.omdbapi.com?apikey=cb876d2a';
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    };
+
+    useEffect(() => {
+        searchMovies('SpiderMan');
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+            <div className="search">
+                <input 
+                    placeholder="Search for movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img 
+                    src={SearchIcon} 
+                    alt="search" 
+                    onClick={() => searchMovies(searchTerm)} 
+                />
+            </div>
+            {movies?.length > 0 ? (
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard key={movie.imdbID} movie={movie} /> // Corrected the component name
+                    ))}
+                </div>
+            ) : (
+                <div className="empty">No movies found</div>
+            )}
+        </div>
+    );
+};
 
 export default App;
